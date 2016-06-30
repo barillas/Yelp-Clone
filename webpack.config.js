76572ff3d05ececf64ae50440bce7a017329eb64
Oscar Barillas <oscarbarillas93@gmail.com>
2016-06-29 23:28:19 -0700

    Added testing dependencies and Webpack config updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,6 @@
+const NODE_ENV = process.env.NODE_ENV;
+const dotenv = require('dotenv');
+
 //Webpack Setup
 const webpack = require('webpack');
 const fs      = require('fs');
@@ -13,18 +16,41 @@ const src     = join(root, 'src');
 const modules = join(root, 'node_modules');
 const dest    = join(root, 'dist');
 
-const NODE_ENV = process.env.NODE_ENV;
-const isDev = NODE_ENV === 'development';
-// alternatively, we can use process.argv[1]
-// const isDev = (process.argv[1] || '')
-//                .indexOf('hjs-dev-server') !== -1;
-
-
 var config = getConfig({
   in: join(src, 'app.js'),
   out: dest,
   clearBeforeBuild: true
 })
+
+
+const isDev = NODE_ENV === 'development';
+
+const dotEnvVars = dotenv.config();
+const environmentEnv = dotenv.config({
+  path: join(root, 'config', `${NODE_ENV}.config.js`),
+  silent: true,
+});
+
+const envVariables =
+    Object.assign({}, dotEnvVars, environmentEnv);
+
+    const defines =
+      Object.keys(envVariables)
+      .reduce((memo, key) => {
+        const val = JSON.stringify(envVariables[key]);
+        memo[`__${key.toUpperCase()}__`] = val;
+        return memo;
+      }, {
+        __NODE_ENV__: JSON.stringify(NODE_ENV)
+      });
+
+
+
+
+config.plugins = [
+  new webpack.DefinePlugin(defines)
+].concat(config.plugins);
+
 
 const cssModulesNames = `${isDev ? '[path][name]__[local]__' : ''}[hash:base64:5]`;
 const matchCssLoaders = /(^|!)(css-loader)($|!)/;
@@ -62,5 +88,14 @@ config.postcss = [].concat([
   require('autoprefixer')({}),
   require('cssnano')({})
 ])
+
+// ...
+config.resolve.root = [src, modules]
+config.resolve.alias = {
+  'css': join(src, 'styles'),
+  'containers': join(src, 'containers'),
+  'components': join(src, 'components'),
+  'utils': join(src, 'utils')
+}
 
 module.exports = config;
